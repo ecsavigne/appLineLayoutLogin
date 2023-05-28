9282529668a2d66b62b9044a9d3ee69f386fa4ae
ecsavigne <ecsavigne@gmail.com>
2023-05-28 01:20:50 -0300

    Eliminado commit de prueba

--- app/src/main/java/com/example/applinelayoutlogin/principal.java
+++ app/src/main/java/com/example/applinelayoutlogin/principal.java
@@ -1,5 +1,5 @@
 package com.example.applinelayoutlogin;
-// Commit de test en desarrollo -> desarrollo 11
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
