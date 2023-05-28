e1a94d03dc43979eef8759ab7d6079e08e733080
ecsavigne <ecsavigne@gmail.com>
2023-05-27 22:19:22 -0300

    ttt
    fdfdsf

--- app/src/main/java/com/example/applinelayoutlogin/principal.java
+++ app/src/main/java/com/example/applinelayoutlogin/principal.java
@@ -1,5 +1,5 @@
 package com.example.applinelayoutlogin;
-
+// Commit de test en desarrollo -> desarrollo
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
